5ee173d7d966f6f2f72d069db0c6065c1c34e1d6
yoeswin Selvaraj E <34303289+yoeswin@users.noreply.github.com>
2020-09-13 16:29:00 +0530

    dashboard

--- public/js/BannerLoader.js
+++ public/js/BannerLoader.js
@@ -1,24 +1,25 @@
 fetch("/banners")
     .then(res => res.json())
-    .then(res => this.doAction(this.formatResponse(res)))
+    .then(data => this.doAction(this.formatResponse(data)))
     .catch(err => err);
 
-
+let aci = "apple";
 //todo action to create cards
-function doAction(banners)
-{
-    for(let i=0;i<banners.length;i++)
-    {
-        console.log(banners[i].description)
-        console.log(banners[i].tournamentId)
-        console.log(banners[i].imageUrl)
+function doAction(banners) {
+
+    for (let i = 0; i < banners.length; i++) {
+
+        //     console.log(banners[i].description)
+        //     console.log(banners[i].tournamentId)
+        //     console.log(banners[i].imageUrl)
+
     }
 }
 
 
-function formatResponse(res)
-{
-    const ta =  Object.keys(res).map(key => ({
+
+function formatResponse(res) {
+    const ta = Object.keys(res).map(key => ({
         ...res[key],
         tournamentID: key
     }));
