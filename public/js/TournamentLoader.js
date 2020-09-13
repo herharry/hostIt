967de04dd94af2c67c7e44e89db56a971475b872
herharry <suriyavadivelan@gmail.com>
2020-09-13 19:25:18 +0530

    initial commit

--- public/js/TournamentLoader.js
+++ public/js/TournamentLoader.js
@@ -1,23 +1,20 @@
-function fetchBanners()
+fetch("/tournament")
+    .then(res => res.json())
+    .then(res => this.loadTournamentInCards(this.formatResponse(res)))
+    .catch(err => err);
+
+function loadTournamentInCards(tournaments)
 {
-    fetch("/banners")
-        .then(res => res.json())
-        .then(res => this.loadBannerInCarousel(this.formatResponse(res)))
-        .catch(err => err);
-}
-
-//todo action to create cards
-function loadBannerInCarousel(banners) {
-
-    for (let i = 0; i < banners.length; i++) {
+    for (let i = 0; i < tournaments.length; i++)
+    {
 
         let itemss = document.createElement("div");
         let img = document.createElement("img");
         img.src = banners[i].imageUrl;
         let att = document.createAttribute("class");
-        
+
         i ? att.value = "carousel-item " : att.value = "carousel-item active" ;
-        
+
         itemss.appendChild(img);
         itemss.setAttributeNode(att);
         document.getElementById("bannerList").appendChild(itemss);
