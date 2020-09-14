a937ab5d71851a6e5949ce71d6edaa0e3701ac67
herharry <suriyavadivelan@gmail.com>
2020-09-14 12:39:48 +0530

    email,password sigini

--- public/js/LoginWithGoogle.js
+++ public/js/LoginWithGoogle.js
@@ -1,18 +1,5 @@
 
-function login(){
-    var firebaseConfig = {
-        apiKey: "AIzaSyBl_PAX0VGLEvIiFWdjWAEazUM7MJEV-1Y",
-        authDomain: "hostitgaming-36a6b.firebaseapp.com",
-        databaseURL: "https://hostitgaming-36a6b.firebaseio.com",
-        projectId: "hostitgaming-36a6b",
-        storageBucket: "hostitgaming-36a6b.appspot.com",
-        messagingSenderId: "916041965514",
-        appId: "1:916041965514:web:88009790aa188d22947933",
-        measurementId: "G-LQMV0QYN8B"
-    };
-
-    firebase.initializeApp(firebaseConfig);
-
+function loginWithGoogle(){
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
     let user;
     const googleAuth = new firebase.auth.GoogleAuthProvider();
@@ -36,7 +23,7 @@ function login(){
         })
         .then(() => {
             console.log("logged in")
-            window.location.assign("/dashboard");
+            window.location.assign("/profile");
 
         });
     return false;
