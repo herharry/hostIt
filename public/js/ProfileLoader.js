19b5665d67f334cfa69056a8a2f6acf3d26e3328
yoeswin Selvaraj E <34303289+yoeswin@users.noreply.github.com>
2020-09-15 13:23:52 +0530

    profile

--- public/js/ProfileLoader.js
+++ public/js/ProfileLoader.js
@@ -1,18 +1,106 @@
-function loadProfileDetails()
-{
+function loadProfileDetails() {
     let user = JSON.parse(sessionStorage.getItem("userInfo"));
     console.log(user)
     setProfileName(user.displayName)
     setProfileImage(user.photoURL)
+    setMobileNumber(user.phoneNumber)
+    setEmail(user.email)
 }
 
 
-function setProfileName(name)
-{
+setProfileName = (name) => {
     document.getElementById("profileName").innerHTML = name;
+    document.getElementById("editProfileName").setAttribute("value", name);
 }
 
-function setProfileImage(image)
-{
-    document.getElementById("profileImage").setAttribute("src",image)
+setProfileImage = (image) => {
+    document.getElementById("profileImage").setAttribute("src", image)
+}
+
+setMobileNumber = (number) => {
+    if (number != null) {
+        document.getElementById("mobileNumber").innerHTML = number;
+        document.getElementById("editMobileNumber").setAttribute("value", number);
+    }
+}
+
+setEmail = (email) => {
+    document.getElementById("profileEmail").innerHTML = email;
+    document.getElementById("editEmail").setAttribute("value", email);
+}
+
+setAccountNo = (number) => {
+    if (number != null) {
+        document.getElementById("editAccountNo").setAttribute("value", number);
+    }
+}
+setIfsc = (number) => {
+    if (number != null) {
+        document.getElementById("mobileNumber").innerHTML = number;
+        document.getElementById("editMobileNumber").setAttribute("value", number);
+    }
+}
+setUpiId = (number) => {
+    if (number) {
+        document.getElementById("mobileNumber").innerHTML = number;
+        document.getElementById("editMobileNumber").setAttribute("value", number);
+    }
+}
+
+// document.getElementById("edittt").addEventListener("click", () => {
+//     showEdit();
+//   });
+removeEdit = () => {
+    document.getElementById("editProfileCard").classList.add("d-none");
+    document.getElementById("myTournament").classList.remove("d-none");
+}
+
+showEdit = () => {
+    document.getElementById("editProfileCard").classList.remove("d-none");
+    document.getElementById("myTournament").classList.add("d-none");
+
+}
+
+updateProfile = () => {
+
+    var name = document.getElementById("editMobileNumber");
+    var mobile = document.getElementById("editMobileNumber");
+    var account = document.getElementById("editAccountNo").value;
+    var ifsc = document.getElementById("editIfsc").value;
+    var upi = document.getElementById("editUpiID").value;
+
+    if ((account && ifsc) || (upi)) {
+
+
+    }
+
+}
+
+
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function isNumberKey(evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode != 46 && charCode > 31 &&
+        (charCode < 48 || charCode > 57))
+        return false;
+    return true;
 }
--- public/js/TournamentLoader.js
+++ public/js/TournamentLoader.js
@@ -6,37 +6,39 @@ function fetchTournaments() {
 }
 
 function loadTournamentInCards(tournaments) {
+    console.log(tournaments)
+
     const cardParent = document.getElementById("tournamentCards")
     for (let i = 0; i < tournaments.length; i++) {
         let card = document.createElement("div");
-            card.className = "card bg-dark";
-            card.style.width = "30rem";
+        card.className = "card bg-dark mx-3 mb-4";
+        card.style.width = "30rem";
         let img = document.createElement("img");
-            // img.src = getGameImage(tournaments[i].gameID)
-            img.className = "card-img-top";
-            img.alt = "";
+        // img.src = getGameImage(tournaments[i].gameID)
+        img.className = "card-img-top";
+        img.alt = "";
         let cardBody = document.createElement("div");
-            cardBody.className = "card-body container";
+        cardBody.className = "card-body container";
         let cardBottom = document.createElement("div");
-            cardBottom.className = "row mb-3 border-bottom";
+        cardBottom.className = "row mb-2 border-bottom";
         let tournamentName = document.createElement("div");
-            tournamentName.className = "col-3 border-right";
-            tournamentName.innerText = tournaments[i].name;
+        tournamentName.className = "col-3 border-right";
+        tournamentName.innerText = tournaments[i].name;
         let amount = document.createElement("div");
-            amount.className = "col-3 border-right";
-            amount.innerText = tournaments[i].amount;
+        amount.className = "col-3 border-right";
+        amount.innerText = tournaments[i].amount;
         let time = document.createElement("div");
-            time.className = "col-3 border-right";
-            let timestamp = tournaments[i].time._seconds * 1000;
-            let tournamentTime = new Date(timestamp).toLocaleTimeString();
-            let tournamentDate = new Date(timestamp).toDateString();
-            time.innerHTML = tournamentDate + "\n" + tournamentTime;
+        time.className = "col-3 border-right";
+        let timestamp = tournaments[i].time._seconds * 1000;
+        let tournamentTime = new Date(timestamp).toLocaleTimeString();
+        let tournamentDate = new Date(timestamp).toDateString();
+        time.innerHTML = tournamentDate + "\n" + tournamentTime;
         let players = document.createElement("div");
-            players.className = "col-3 border-right";
-            players.innerHTML = tournaments[i].vacantSeats + "/" + tournaments[i].totalSeats;
+        players.className = "col-3 border-right";
+        players.innerHTML = tournaments[i].vacantSeats + "/" + tournaments[i].totalSeats;
         let button = document.createElement("a");
-            button.className = "btn btn-primary m-1";
-            button.text = "Join"
+        button.className = "btn btn-primary m-1";
+        button.text = "Join"
 
         card.appendChild(img);
         card.appendChild(cardBody);
