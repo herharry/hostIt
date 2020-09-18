199840cce045feb973920404783da8f1d8e0019b
herharry <suriyavadivelan@gmail.com>
2020-09-18 16:01:30 +0530

    real time tournament updates

--- public/js/Constants.js
+++ public/js/Constants.js
@@ -9,6 +9,8 @@ const firebaseConfig = {
     measurementId: "G-LQMV0QYN8B"
 };
 firebase.initializeApp(firebaseConfig);
+const DB= firebase.firestore();
+const USER_IN_SESSION = JSON.parse(sessionStorage.getItem("userInfo"));
 
 
 function sessionLogin(result){
@@ -28,4 +30,30 @@ function sessionLogin(result){
         sessionStorage.setItem("userInfo", JSON.stringify(user))
         window.location.assign("/profile");
     });
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exhours) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exhours*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function delete_cookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
--- public/js/TournamentLoader.js
+++ public/js/TournamentLoader.js
@@ -1,62 +1,122 @@
-function fetchTournaments() {
-    fetch("/tournament")
-        .then(res => res.json())
-        .then(res => this.loadTournamentInCards(this.formatResponse(res)))
-        .catch(err => err);
+// FIREBASE AUTHENTICATION FOR THE CURRENT USER STARTS*****************************************************************************
+
+async function loadTournamentJS()
+{
+    let uid = USER_IN_SESSION.uid;
+    if(getCookie("SU_SY") == "")
+    {
+        await fetch("/createToken", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({uid}),
+        }).then(res => res.text()).then(function(res){
+            firebase.auth().signInWithCustomToken(res.toString()).then(function (user)
+            {
+                setCookie("SU_SY",res.toString(),1);
+                tournamentListener();
+            })
+        });
+    }
+    else
+    {
+        firebase.auth().signInWithCustomToken(getCookie("SU_SY")).then(function (user)
+        {
+            console.log("hey")
+            tournamentListener();
+        }).catch(reason => {
+            console.log(reason);
+            delete_cookie("SU_SY");
+            loadProfileJS();
+        });
+    }
 }
 
+function tournamentListener()
+{
+    DB.collection("Tournaments").where("isFinished","==",false)
+        .onSnapshot(function(snapshot) {
+            snapshot.forEach(function (doc)
+            {
+                let tournament={};
+                tournament = doc.data();
+                tournament.id = doc.id;
+                let flag = document.getElementById("CARD"+tournament.id);
+                if(typeof(flag) != 'undefined' && flag!=null)
+                {
+                    loadTournamentInExistingCard(tournament)
+                }
+                else {
+                    loadTournamentInNewCard(tournament)
 
-function loadTournamentInCards(tournaments) {
-    console.log(tournaments)
+                }
+            })
+        });
+    console.log("helos")
+}
+//FIREBASE AUTHENTICATION FOR THE CURRENT USER ENDS *****************************************************************************
+
+
+function loadTournamentInNewCard(tournament) {
+    console.log(tournament)
 
     const cardParent = document.getElementById("tournamentCards")
-    for (let i = 0; i < tournaments.length; i++) {
         let card = document.createElement("div");
         card.className = "card p-0 bg-dark my-2 mx-2 col-12 col-lg-6";
+        card.id="CARD"+tournament.id;
         // let img = document.createElement("img");
         // img.src = getGameImage(tournaments[i].gameID)
         // img.className = "card-img-top";
         // img.alt = "";
         let tournamentNames = document.createElement("h3");
+        tournamentNames.id = "NAMES"+tournament.id;
         tournamentNames.className = "card-title text-upper";
-        tournamentNames.innerText = tournaments[i].name;
+        tournamentNames.innerText = tournament.name;
         let cardBody = document.createElement("div");
         cardBody.className = "card-body container-fluid py-2";
         let cardBottom = document.createElement("div");
         cardBottom.className = "row mb-2 border-bottom";
         let tournamentName = document.createElement("div");
+        tournamentName.id = "NAME"+tournament.id;
         tournamentName.className = "col-3 p-1 border-right";
-        tournamentName.innerText = tournaments[i].name;
+        tournamentName.innerText = tournament.name;
         let amount = document.createElement("div");
+        amount.id = "AMOUNT"+tournament.id;
         amount.className = "col-2 text-center p-1 border-right";
-        amount.innerText = tournaments[i].amount;
+        amount.innerText = tournament.amount;
         let time = document.createElement("div");
+        time.id = "TIME"+tournament.id;
         time.className = "col-5 p-1 border-right";
-        let timestamp = tournaments[i].time._seconds * 1000;
+        let timestamp = tournament.time._seconds * 1000;
         let tournamentTime = new Date(timestamp).toLocaleTimeString();
         let tournamentDate = new Date(timestamp).toDateString();
         time.innerHTML = tournamentDate + "\n" + tournamentTime;
         let players = document.createElement("div");
+        players.id = "PLAYERS"+tournament.id;
         players.className = "col-2 text-center p-1";
-        players.innerHTML = (tournaments[i].totalSeats - tournaments[i].vacantSeats) + "/" + tournaments[i].totalSeats;
+        players.innerHTML = (tournament.totalSeats - tournament.vacantSeats) + "/" + tournament.totalSeats;
         let progress = document.createElement("div");
         progress.className = "progress mt-4";
         let progressBar = document.createElement("div");
+        progressBar.id = "PROGRESS_BAR"+tournament.id;
         progressBar.className = "progress-bar bg-dark progress-gradient";
         progressBar.setAttribute("role","progressbar");
-        let percent = ((tournaments[i].totalSeats-tournaments[i].vacantSeats) / tournaments[i].totalSeats)*100;
+        let percent = ((tournament.totalSeats-tournament.vacantSeats) / tournament.totalSeats)*100;
         progressBar.setAttribute("style","width :" +percent+"%");
         progressBar.setAttribute("role","progressbar");
         progressBar.innerHTML = percent+"% full";
         let remainig = document.createElement("p");
+        remainig.id = "REMAINING"+tournament.id;
         remainig.className ="float-left text-small"
-        remainig.innerHTML = tournaments[i].vacantSeats+" remaining";
+        remainig.innerHTML = tournament.vacantSeats+" remaining";
 
-        
+
         let button = document.createElement("a");
         button.className = "btn btn-primary px-4 py-1 mt-3 mx-3 float-right";
         button.text = "Join"
-        button.id = tournaments[i].tid;
+        button.id = tournament.tid;
         button.setAttribute("onClick","loadSpecificTournament(this.id)");
 
 
@@ -74,8 +134,32 @@ function loadTournamentInCards(tournaments) {
         cardBody.appendChild(remainig)
         cardBody.appendChild(button);
         cardParent.appendChild(card);
-    }
 }
+
+
+function loadTournamentInExistingCard(tournament) {
+
+    let tournamentNames = document.getElementById("NAMES"+tournament.id);
+    tournamentNames.innerText = tournament.name;
+    let tournamentName = document.getElementById("NAME"+tournament.id);
+    tournamentName.innerText = tournament.name;
+    let amount = document.getElementById("AMOUNT"+tournament.id);
+    amount.innerText = tournament.amount;
+    let time = document.getElementById("TIME"+tournament.id);
+    let timestamp = tournament.time._seconds * 1000;
+    let tournamentTime = new Date(timestamp).toLocaleTimeString();
+    let tournamentDate = new Date(timestamp).toDateString();
+    time.innerHTML = tournamentDate + "\n" + tournamentTime;
+    let players = document.getElementById("PLAYERS"+tournament.id);
+    players.innerHTML = (tournament.totalSeats - tournament.vacantSeats) + "/" + tournament.totalSeats;
+    let progressBar = document.getElementById("PROGRESS_BAR"+tournament.id);
+    let percent = ((tournament.totalSeats-tournament.vacantSeats) / tournament.totalSeats)*100;
+    progressBar.setAttribute("style","width :" +percent+"%");
+    progressBar.innerHTML = percent+"% full";
+    let remainig = document.getElementById("REMAINING"+tournament.id);
+    remainig.innerHTML = tournament.vacantSeats+" remaining";
+}
+
 
 function formatResponse(res) {
     const ta = Object.keys(res).map(key => ({
