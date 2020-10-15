5e15eb32993f0a186013b12c1aa49c0dce027eae
herharry <suriyavadivelan@gmail.com>
2020-10-15 18:50:41 +0530

    users collection fix

--- public/js/ProfileLoader.js
+++ public/js/ProfileLoader.js
@@ -295,8 +295,11 @@ function createUserInCollection() {
             user.walletAmount = document.getElementById("winnings").innerHTML;
             user.role = 0;
             user.profileImageURL = document.getElementById("profileImage").getAttribute("src");
-            user.mobileNo = getElementValue("editMobileNumber");
-            user.vpa = getElementValue("editUpiID");
+            user.mobileNo = getElementValue("editMobileNumber").toNumber();
+            let vpa = {};
+            vpa.vpa = getElementValue("editUpiID");
+            user.vpa = vpa;
+            user.token = "";
             let bankDetail = {};
             bankDetail.accountNo = getElementValue("editAccountNo");
             bankDetail.ifsc = getElementValue("editIfsc");
